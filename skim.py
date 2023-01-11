067a8db54ff328eb459df2a18f55724ca42cf1db
Daniele Guido <gui.daniele@gmail.com>
2023-01-11 12:15:28 +0100

    Update skim.py

--- skim.py
+++ skim.py
@@ -3,6 +3,7 @@ import sys
 import json
 import fire
 
+
 def main(notebook=None):
     if notebook is None:
       print('::error::No path provided')
@@ -12,10 +13,16 @@ def main(notebook=None):
     if not os.path.exists(notebook_filepath):
       print(f'::error::Path {notebook_filepath} does not exist')
       sys.exit(1)
+
+    print(f'::debug::Path {notebook_filepath} exists')
+    
     with open(notebook_filepath) as f: 
       data = json.load(f)
       cells = data.get('cells', [])
-      print(f"::set-output name=size::{len(cells)}")
-    
+      print(f"::debug::num of cells: {len(cells)}")
+      
+      with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'size={len(cells)}', file=fh)
+
 if __name__ == "__main__":
     fire.Fire(main)
